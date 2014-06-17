64bdf5223e164b88fc0ac94c3841318d6bf3f404
hajipion <hajiming@gmail.com>
2014-06-18 08:21:39 +0900

    Hyper commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  // スタートを押すと
+  $("#startBtn").click(function() {
+    $(".firstarea").css("display", "none");
+    $(".secondarea").css("display", "block");
+  });
+
+  // 決勝にする
+  $(".title").click(function() {
+    $(".title img").attr("src", "images/final.png");
+  });
+})
